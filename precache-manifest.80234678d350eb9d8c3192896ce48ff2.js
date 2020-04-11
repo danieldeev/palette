534e28d5da3278dc800131b1fd96e81e3b758e99
root <conteudoalemdoolhar@gmail.com>
2020-04-11 17:31:45 -0500

    first deploy

---
+++ precache-manifest.80234678d350eb9d8c3192896ce48ff2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "755958cd0781585e682acc101f3c21b7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "20f819b1405a496e16aa",
+    "url": "./static/css/main.4b8cfa70.chunk.css"
+  },
+  {
+    "revision": "5fe255dc0486568821b9",
+    "url": "./static/js/2.dabdc399.chunk.js"
+  },
+  {
+    "revision": "b3924902b6517db7f2b56facf8ba237b",
+    "url": "./static/js/2.dabdc399.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20f819b1405a496e16aa",
+    "url": "./static/js/main.504c9439.chunk.js"
+  },
+  {
+    "revision": "51df773bbfdd3edaf5d3",
+    "url": "./static/js/runtime-main.513eede3.js"
+  },
+  {
+    "revision": "e00e2a77dd88a8fe75573a5d993af76a",
+    "url": "./static/media/Ubuntu-Bold.e00e2a77.ttf"
+  },
+  {
+    "revision": "277289c53af7cb469c1dc5dca3adca35",
+    "url": "./static/media/Ubuntu-Light.277289c5.ttf"
+  },
+  {
+    "revision": "d7e60f9d1433a45ed71817f6d23abeca",
+    "url": "./static/media/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.d7e60f9d.woff2"
+  }
+]);
